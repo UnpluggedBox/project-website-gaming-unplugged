f49599f9ec849e474b85a6773dd0f34dcddf4648
henrytanujaya <70674841+henrytanujaya@users.noreply.github.com>
2020-10-05 12:23:31 +0700

    ga tau

--- pages/js/login.js
+++ pages/js/login.js
@@ -2,26 +2,26 @@ var uname = document.forms['form']['uname']
 var psw = document.forms['form']['psw']
 
 uname.addEventListener('textInput', user_verify)
-psw.addEventListener('textInput', pass_verify)
+psw.addEventListener('textInput', psw_verify)
 
 function valid() {
-    if (uname != 'admin') {
+    if (uname.value.length < 5 && uname.value.length > 5) {
         uname.focus();
         return false
     }
-    if (psw != 'admin') {
+    if (psw.value.length < 5 && psw.value.length > 5) {
         psw.focus();
         return false
     }
 }
 
 function user_verify() {
-    if (uname == 'admin') {
+    if (uname.value.length == 5) {
         return true;
     }
 }
-function pass_verify() {
-    if (psw == 'admin') {
+function psw_verify() {
+    if (psw.value.length == 5) {
         return true;
     }
 }
