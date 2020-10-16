a9b2973e75405ef1f0471ccce39c31748ebc8b3e
UnpluggedBox <rubinsalim17@gmail.com>
2020-10-17 05:54:36 +0700

    v0.4 Release
    -Added a Registration page
    -Addes Games page with search function
    -Changed the whole site except the first homepage to look like as if a user is logged in
    -Minor typo bug fixes

--- pages/js/Register.js
+++ pages/js/Register.js
@@ -2,11 +2,11 @@ var check = function() {
     if (document.getElementById('pass').value ==
       document.getElementById('passrepeat').value) {
       document.getElementById('message').style.color = 'green';
-      document.getElementById('message').innerHTML = 'matching';
+      document.getElementById('message').innerHTML = 'Passwords match';
       document.getElementById('submit').disabled = false;
     } else {
       document.getElementById('message').style.color = 'red';
-      document.getElementById('message').innerHTML = 'not matching';
+      document.getElementById('message').innerHTML = 'Passwords doesnt match!';
       document.getElementById('submit').disabled = true;
     }
   }
