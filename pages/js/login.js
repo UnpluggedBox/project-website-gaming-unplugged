43e53fa243986fc1bf22e6b747f05aee8c1f6def
henrytanujaya <70674841+henrytanujaya@users.noreply.github.com>
2020-10-17 11:43:05 +0700

    v0.4 Release

--- pages/js/login.js
+++ pages/js/login.js
@@ -1,27 +1,8 @@
-var uname = document.forms['form']['uname']
-var psw = document.forms['form']['psw']
-
-uname.addEventListener('textInput', user_verify)
-psw.addEventListener('textInput', psw_verify)
-
-function valid() {
-    if (uname.value.length < 5 && uname.value.length > 5) {
-        uname.focus();
-        return false
+var checklogin = function() {
+    if (document.getElementById('uname').value ==
+      document.getElementById('psw').value) {
+      document.getElementById('login').disabled = false;
+    } else {
+      document.getElementById('login').disabled = true;
     }
-    if (psw.value.length < 5 && psw.value.length > 5) {
-        psw.focus();
-        return false
-    }
-}
-
-function user_verify() {
-    if (uname.value.length == 5) {
-        return true;
-    }
-}
-function psw_verify() {
-    if (psw.value.length == 5) {
-        return true;
-    }
-}
+  }
