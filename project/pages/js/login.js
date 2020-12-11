1df2938d44a736daea2f607c48e063b4dd5b63d2
UnpluggedBox <rubinsalim17@gmail.com>
2020-12-11 20:05:14 +0700

    Clean up files to free up space

--- project/pages/js/login.js
+++
@@ -1,9 +0,0 @@
-var checklogin = function() {
-    if (document.getElementById('uname').value == 'admin' && document.getElementById('psw').value == 'admin') {
-      return true
-    } else {
-      document.getElementById('messagelogin').style.color = 'red';
-      document.getElementById('messagelogin').innerHTML = 'Wrong Username or Password';
-      return false
-    }
-  }
